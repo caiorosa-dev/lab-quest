53493f5e85601ce51f1cd8a8601b5a2e5a7bf8b7
Redakas <andregabrielmelo@gmail.com>
2024-12-04 13:47:52 -0300

    feat: Adicionando barra de navegação

--- apps/web/src/components/full-screen-page.tsx
+++ apps/web/src/components/full-screen-page.tsx
@@ -1,13 +1,22 @@
-import { cn } from '@/helpers/utils';
-import { PropsWithChildren } from 'react';
+import { cn } from "@/helpers/utils";
+import { PropsWithChildren } from "react";
+import { BottomNav } from "./bottom-tab-navigation";
 
 export function FullScreenPage({
   children,
   className,
 }: PropsWithChildren<{ className?: string }>) {
   return (
-    <main className={cn('w-full min-h-screen h-full flex justify-center', className)}>
-      {children}
-    </main>
+    <>
+      <main
+        className={cn(
+          "w-full min-h-screen h-full flex justify-center",
+          className
+        )}
+      >
+        {children}
+      </main>
+      <BottomNav></BottomNav>
+    </>
   );
 }
